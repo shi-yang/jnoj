2beca7e0f1d4e57434806d1c58c17238bb1eca23
shi-yang <dr@shiyang.me>
2018-04-06 22:58:55 +0800

    change default db example

--- config/db.php
+++ config/db.php
@@ -2,13 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ee',
+    'dsn' => 'mysql:host=localhost;dbname=jnoj',
     'username' => 'root',
     'password' => '123456',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
 ];
