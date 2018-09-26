863d37d55c7a5cf8133394becc3f2e459b352e15
shi-yang <dr@shiyang.me>
2018-09-27 06:39:14 +0800

    update

--- polygon/src/okcalls64.h
+++ polygon/src/okcalls64.h
@@ -17,9 +17,9 @@
  * along with HUSTOJ. if not, see <http://www.gnu.org/licenses/>.
  */
 //c & c++
-int LANG_CV[256] = {0,1,2,3,4,5,8,9,11,12,20,21,59,63,89,158,202,231,240, 272, SYS_time, SYS_read, SYS_uname, SYS_write, SYS_open,
-		SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_munmap, SYS_mprotect,
-		SYS_mmap, SYS_fstat, SYS_set_thread_area, 252, SYS_arch_prctl, 0 };
+int LANG_CV[256] = {0,1,2,3,4,5,8,9,11,12,20,21,59,63,89,158,231,240,272,511, SYS_time, SYS_read, SYS_uname, SYS_write, SYS_open,
+                    		SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_munmap, SYS_mprotect,
+                    		SYS_mmap, SYS_fstat, SYS_set_thread_area, 252, SYS_arch_prctl, 0 };
 //java
 int LANG_JV[256] = {0,2,3,4,5,9,10,11,12,13,14,21,56,59,89,97,104,158,202,218,231,273,257, 
 		61, 22, 6, 33, 8, 13, 16, 111, 110, 39, 79, 302, SYS_fcntl,
@@ -30,15 +30,14 @@ int LANG_JV[256] = {0,2,3,4,5,9,10,11,12,13,14,21,56,59,89,97,104,158,202,218,23
 		SYS_getuid, SYS_getgid, SYS_geteuid, SYS_getegid, SYS_set_thread_area,
 		SYS_set_tid_address, SYS_set_robust_list, SYS_exit_group, 158, 0 };
 //python
-int LANG_YV[256] = {0,1,2,3,4,5,6,8,9,10,12,13,14,16,20,21,59,78,79,89,97,99,102,104,107,108,158,218,231,273,32,72,131,
-		41, 42, 146, SYS_mremap, 158, 117, 60, 39, 102, 191,318,
-		SYS_access, SYS_arch_prctl, SYS_brk, SYS_close, SYS_execve,
-		SYS_exit_group, SYS_fcntl, SYS_fstat, SYS_futex, SYS_getcwd,
-		SYS_getdents, SYS_getegid, SYS_geteuid, SYS_getgid, SYS_getrlimit,
-		SYS_getuid, SYS_ioctl, SYS_lseek, SYS_lstat, SYS_mmap, SYS_mprotect,
-		SYS_munmap, SYS_open, SYS_read, SYS_readlink, SYS_rt_sigaction,
-		SYS_rt_sigprocmask, SYS_set_robust_list, SYS_set_tid_address, SYS_stat,
-		SYS_write, 0 };
+int LANG_YV[256] = {0,2,3,4,5,6,8,9,10,11,12,13,14,16,21,32,59,72,78,79,89,97,99,102,104,107,108,131,158,218,228,231,273,318,39,99,302,99,32,72,131,1,202,257,41, 42, 146, SYS_mremap, 158, 117, 60, 39, 102, 191,
+                    		SYS_access, SYS_arch_prctl, SYS_brk, SYS_close, SYS_execve,
+                    		SYS_exit_group, SYS_fcntl, SYS_fstat, SYS_futex, SYS_getcwd,
+                    		SYS_getdents, SYS_getegid, SYS_geteuid, SYS_getgid, SYS_getrlimit,
+                    		SYS_getuid, SYS_ioctl, SYS_lseek, SYS_lstat, SYS_mmap, SYS_mprotect,
+                    		SYS_munmap, SYS_open, SYS_read, SYS_readlink, SYS_rt_sigaction,
+                    		SYS_rt_sigprocmask, SYS_set_robust_list, SYS_set_tid_address, SYS_stat,
+                    		SYS_write, 0 };
 
 struct ok_call {
 	int * call;
