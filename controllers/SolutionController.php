7091c023c583828b37a099a79a872bd430a0f4cf
shi-yang <dr@shiyang.me>
2018-10-04 15:25:09 +0800

    update

--- controllers/SolutionController.php
+++ controllers/SolutionController.php
@@ -57,8 +57,11 @@ class SolutionController extends Controller
         $this->layout = false;
         $model = $this->findModel($id);
 
-        // 验证是否有权限查看
-        if ($model->contest_id != null && $model->status == Solution::STATUS_HIDDEN) {
+        // 验证是否有权限查看。以下代码中 isShareCode 的说明参见 config\params.php 文件。
+        // 当系统允许用户可以查看其他用户的代码时，此时只限制比赛过程中不能被查看。
+        if (($model->contest_id != null && $model->status == Solution::STATUS_HIDDEN) ||
+            !Yii::$app->params['isShareCode']) {
+            // 未登录用户不能查看，不是自己提交的记录且不是管理员的情况下不能查看
             if (Yii::$app->user->isGuest
                 || ($model->created_by != Yii::$app->user->id && Yii::$app->user->identity->role != User::ROLE_ADMIN)) {
                 throw new ForbiddenHttpException('You are not allowed to perform this action.');
@@ -70,7 +73,7 @@ class SolutionController extends Controller
     }
 
     /**
-     * Displays result of a single Solution model.
+     * 提交记录的出错信息。如因 Wrong Answer、Runtime Error、Compile Error 所记录的信息
      * @param integer $id
      * @return mixed
      * @throws ForbiddenHttpException if the model cannot be viewed.
@@ -80,12 +83,13 @@ class SolutionController extends Controller
         $this->layout = false;
         $model = $this->findModel($id);
 
-
-        // 验证是否有权限查看
-        if ($model->contest_id != null && $model->status == Solution::STATUS_HIDDEN) {
+        // 验证是否有权限查看。以下代码中 isShareCode 的说明参见 config\params.php 文件。
+        // 对于比赛中的提交记录，只允许查看 Compile Error。
+        if ($model->contest_id != null && $model->status == Solution::STATUS_HIDDEN ||
+            !Yii::$app->params['isShareCode']) {
             $role = true;
             if (!Yii::$app->user->isGuest) {
-                $role =Yii::$app->user->identity->role != User::ROLE_ADMIN;
+                $role = Yii::$app->user->identity->role != User::ROLE_ADMIN;
             }
             if (Yii::$app->user->isGuest
                 || ($model->created_by != Yii::$app->user->id && $role)
@@ -109,7 +113,8 @@ class SolutionController extends Controller
         $this->layout = 'main';
         $model = $this->findModel($id);
 
-        // 验证是否有权限查看
+        // 验证是否有权限查看。以下代码中 isShareCode 的说明参见 config\params.php 文件。
+        // 对于比赛中的提交记录，只允许 Compile Error 的情况下打开该页面。
         if ($model->contest_id != null && $model->status == Solution::STATUS_HIDDEN) {
             $role = true;
             if (!Yii::$app->user->isGuest) {
