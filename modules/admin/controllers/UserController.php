99031d76668fd227ed9f6d32fbfa3a543b6829f5
shi-yang <dr@shiyang.me>
2018-11-17 10:13:47 +0800

    update

--- modules/admin/controllers/UserController.php
+++ modules/admin/controllers/UserController.php
@@ -35,7 +35,7 @@ class UserController extends Controller
                 'ruleConfig' => [
                     'class' => AccessRule::className(),
                 ],
-                'only' => ['index', 'create', 'update', 'delete'],
+                'only' => ['index', 'create', 'update', 'delete', 'view'],
                 'rules' => [
                     [
                         'actions' => ['index', 'view', 'create', 'update', 'delete'],
--- modules/admin/views/user/view.php
+++ modules/admin/views/user/view.php
@@ -30,11 +30,6 @@ $this->params['breadcrumbs'][] = $this->title;
         'attributes' => [
             'id',
             'username',
-            'auth_key',
-            'password_hash',
-            'password_reset_token',
-            'email:email',
-            'status',
             'created_at',
             'updated_at',
         ],
