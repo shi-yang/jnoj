8e270d89c319e58187bc783c09b752eb18af2191
shi-yang <dr@shiyang.me>
2018-11-24 19:56:36 +0800

    update

--- views/layouts/contest.php
+++ views/layouts/contest.php
@@ -23,8 +23,8 @@ $status = $model->getRunStatus();
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
