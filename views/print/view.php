1f2245f9ea9d31dbcd1f6f64be932b43fcf616d2
shi-yang <dr@shiyang.me>
2018-11-24 20:20:05 +0800

    支持浏览器打印

--- views/print/view.php
+++ views/print/view.php
@@ -10,11 +10,20 @@ $this->title = $model->user->nickname;
 $this->params['breadcrumbs'][] = ['label' => $model->contest->title, 'url' => ['/contest/view', 'id' => $model->contest_id]];
 $this->params['breadcrumbs'][] = ['label' => 'Print Sources', 'url' => ['index', 'id' => $model->contest_id]];
 $this->params['breadcrumbs'][] = $this->title;
+
+$this->registerCss('
+@media print {
+.breadcrumb {
+    display: none;
+}
+}
+');
 ?>
 <div class="print-source-view">
-
-    <h1><span class="glyphicon glyphicon-user"></span> <?= Html::encode($this->title) ?>[<?= Html::encode($model->user->username) ?>]</h1>
-    <p>
+    <h1>
+        <span class="glyphicon glyphicon-user"></span> <?= Html::encode($this->title) ?>[<?= Html::encode($model->user->username) ?>]
+    </h1>
+    <p class="hidden-print">
         <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
@@ -24,6 +33,11 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
         ]) ?>
     </p>
+    <?php if (Yii::$app->user->identity->role === \app\models\User::ROLE_ADMIN): ?>
+        <h3 class="hidden-print text-muted">
+            注：可以使用浏览器打印功能来快速打印（对于 Chrome 浏览器：页面上右键-打印。对于火狐浏览器：点击在右上角菜单按钮，然后选择打印）。
+        </h3>
+    <?php endif; ?>
     <hr>
     <p><span class="glyphicon glyphicon-time"></span> <?= Yii::$app->formatter->asRelativeTime($model->created_at) ?></p>
     <pre><p><?= Html::encode($model->source) ?></p></pre>
