a0d8593f2515ce02bc12161b6bc80e007e57f1cc
shi-yang <dr@shiyang.me>
2018-12-20 13:33:24 +0800

    update

--- modules/polygon/views/problem/spj.php
+++ modules/polygon/views/problem/spj.php
@@ -17,16 +17,21 @@ $model->setSamples();
 <p>
     如果该题目需要特判的，请在下面填写特判程序。参考：<?= Html::a('如何编写特判程序？', ['/wiki/problem']) ?>
 </p>
-<?php $form = ActiveForm::begin(); ?>
+
+<?php if ($model->spj): ?>
+    <?php $form = ActiveForm::begin(); ?>
 
 
-<?= $form->field($model, 'spj_lang')->textInput([
-    'maxlength' => true, 'value' => 'C、C++', 'disabled' => true
-])->hint('当前仅支持 C\C++ 语言。') ?>
+    <?= $form->field($model, 'spj_lang')->textInput([
+        'maxlength' => true, 'value' => 'C、C++', 'disabled' => true
+    ])->hint('当前仅支持 C\C++ 语言。') ?>
 
-<?= $form->field($model, 'spj_source')->widget('app\widgets\codemirror\CodeMirror'); ?>
+    <?= $form->field($model, 'spj_source')->widget('app\widgets\codemirror\CodeMirror'); ?>
 
-<div class="form-group">
-    <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-primary']) ?>
-</div>
-<?php ActiveForm::end(); ?>
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-primary']) ?>
+    </div>
+    <?php ActiveForm::end(); ?>
+<?php else: ?>
+    <p>当前题目不是 SPJ 判题，如需启用 SPJ 判题，请先到题目信息编辑页面将 Special Judge 改为是。</p>
+<?php endif; ?>
