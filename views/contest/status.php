665828818ae840cf51554b0120ef6cfcb909d43b
shi-yang <dr@shiyang.me>
2019-03-18 22:10:10 +0800

    update

--- views/contest/status.php
+++ views/contest/status.php
@@ -22,7 +22,7 @@ foreach ($problems as $key => $p) {
 ?>
 <div class="solution-index" style="margin-top: 20px">
     <?php if (!empty($model->lock_board_time) && strtotime($model->lock_board_time) <= time() &&
-              strtotime($model->end_time) >= time() - Yii::$app->setting('scoreboardFrozenTime')) :?>
+              strtotime($model->end_time) >= time() - Yii::$app->setting->get('scoreboardFrozenTime')) :?>
         <p class="text-center">现已是封榜状态，榜单将不再实时更新，只显示封榜前的提交及您个人的所有提交记录。</p>
     <?php endif; ?>
     <?php Pjax::begin() ?>
