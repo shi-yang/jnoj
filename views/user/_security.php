e2e92b91cd6549a3c6b5afcc677d809c4f2b9c58
shi-yang <i@shiyang.me>
2020-01-03 13:30:58 +0800

    密码框

--- views/user/_security.php
+++ views/user/_security.php
@@ -10,11 +10,11 @@ use yii\widgets\ActiveForm;
 
 <?php $form = ActiveForm::begin(); ?>
 
-<?= $form->field($model, 'oldPassword')->textInput() ?>
+<?= $form->field($model, 'oldPassword')->passwordInput() ?>
 
-<?= $form->field($model, 'newPassword')->textInput() ?>
+<?= $form->field($model, 'newPassword')->passwordInput() ?>
 
-<?= $form->field($model, 'verifyPassword')->textInput() ?>
+<?= $form->field($model, 'verifyPassword')->passwordInput() ?>
 
 <div class="form-group">
     <?= Html::submitButton(Yii::t('app', 'Update'), ['class' => 'btn btn-primary']) ?>
