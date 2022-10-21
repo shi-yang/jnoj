c9773f0e54075d8e93e398078caee9cf1488d5b6
shiyang <dr@shiyang.me>
2022-10-22 00:33:43 +0800

    定义接口

--- web/src/pages/problem/update/info.tsx
+++ web/src/pages/problem/update/info.tsx
@@ -1,5 +1,20 @@
-export default () => {
+import { Form, Input, Button, Checkbox } from '@arco-design/web-react';
+const FormItem = Form.Item;
+
+const App = () => {
   return (
-    <div>preview</div>
-  )
-}
+    <Form style={{ width: 600 }} autoComplete='off'>
+      <FormItem label='时间限制'>
+        <Input />
+      </FormItem>
+      <FormItem label='内存限制'>
+        <Input />
+      </FormItem>
+      <FormItem wrapperCol={{ offset: 5 }}>
+        <Button type='primary'>保存</Button>
+      </FormItem>
+    </Form>
+  );
+};
+
+export default App;
