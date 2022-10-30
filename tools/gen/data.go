2e2023bf1dc6f83ed2a25492a56b4a369d92e4f8
shiyang <dr@shiyang.me>
2022-10-30 22:38:14 +0800

    优化增删改查代码生成工具

--- tools/gen/data.go
+++ tools/gen/data.go
@@ -17,7 +17,12 @@ type dataDesc struct {
 func (s *dataDesc) execute() string {
 	buf := new(bytes.Buffer)
 	funcMap := template.FuncMap{
-		"tolower": strings.ToLower,
+		"tolower": func(s string) string {
+			if len(s) == 0 {
+				return ""
+			}
+			return strings.ToLower(string(s[0])) + s[1:]
+		},
 	}
 	tmpl, err := template.New("data").
 		Funcs(funcMap).
