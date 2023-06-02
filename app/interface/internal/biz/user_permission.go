315a13ec3788c441c37a3cfa9ca650cae45e35b6
shiyang <dr@shiyang.me>
2023-06-02 16:52:39 +0800

    refactor: 重构角色-资源关系

--- app/interface/internal/biz/user_permission.go
+++ app/interface/internal/biz/user_permission.go
@@ -19,46 +19,40 @@ func init() {
 
 // 定义访问权限的结构体
 type AccessControl struct {
-	resourceToRoles map[int][]int // 资源对应的可访问角色列表
+	roleToResources map[int][]int // 角色对应的可访问资源列表
 }
 
 // 初始化访问权限
 func NewAccessControl() *AccessControl {
 	ac := &AccessControl{
-		resourceToRoles: make(map[int][]int),
+		roleToResources: make(map[int][]int),
 	}
-	// 设置 Admin 所有资源可供 超级管理员访问
-	ac.resourceToRoles[ResourceAdmin] = []int{UserRoleSuperAdmin}
+	// 设置 超级管理员 可访问资源
+	ac.roleToResources[UserRoleSuperAdmin] = []int{ResourceAdmin, ResourceSubmission, ResourceProblem, ResourceContest, ResourceGroup}
 
-	// 设置 Submission 所有资源可供 官方用户、管理员、超级管理员访问
-	ac.resourceToRoles[ResourceSubmission] = []int{UserRoleOfficial, UserRoleAdmin, UserRoleSuperAdmin}
+	// 设置 管理员 可访问资源
+	ac.roleToResources[UserRoleAdmin] = []int{ResourceSubmission, ResourceContest, ResourceGroup, ResourceProblem}
 
-	// 设置 Contest 所有资源可供 管理员、超级管理员访问
-	ac.resourceToRoles[ResourceContest] = []int{UserRoleAdmin, UserRoleSuperAdmin}
-
-	// 设置 Group 所有资源可供 管理员、超级管理员访问
-	ac.resourceToRoles[ResourceGroup] = []int{UserRoleAdmin, UserRoleSuperAdmin}
-
-	// 设置 Problem 所有资源可供 管理员、超级管理员访问
-	ac.resourceToRoles[ResourceProblem] = []int{UserRoleAdmin, UserRoleSuperAdmin}
+	// 设置 官方用户可访问资源
+	ac.roleToResources[UserRoleOfficial] = []int{ResourceSubmission, ResourceContest, ResourceGroup}
 	return ac
 }
 
 // 检查指定角色是否有访问指定资源的权限
 func CheckAccess(role int, resource int) bool {
-	roles, ok := accessControl.resourceToRoles[resource]
+	resources, ok := accessControl.roleToResources[role]
 	if !ok {
-		// 如果资源没有对应的可访问角色，则默认任何角色都无法访问
+		// 如果角色没有对应的可访问资源，则默认任何资源都无法访问
 		return false
 	}
 
-	for _, r := range roles {
-		if r == role {
-			// 如果指定角色在可访问角色列表中，则有权限访问该资源
+	for _, r := range resources {
+		if r == resource {
+			// 如果指定资源在可访问资源列表中，则有权限访问该资源
 			return true
 		}
 	}
 
-	// 指定角色不在可访问角色列表中，则无权限访问该资源
+	// 指定资源不在可访问资源列表中，则无权限访问该资源
 	return false
 }
