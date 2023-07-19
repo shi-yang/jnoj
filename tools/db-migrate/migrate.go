383924a9a1477342f13f66c9e2b3fcac17ea3fda
shi-yang <dr@shiyang.me>
2023-07-20 00:22:50 +0800

    feat: 增加详细的个人信息

--- tools/db-migrate/migrate.go
+++ tools/db-migrate/migrate.go
@@ -13,12 +13,55 @@ func Migrate(db *gorm.DB) {
 	m := gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
 		MigrateAddSuperAdminUser20230716(),
 		MigrateAddUserBadge20230717(),
+		MigrateAddUserProfile20230719(),
 	})
 	m.InitSchema(MigrateInitDB)
 	if err := m.Migrate(); err != nil {
 		log.Fatalf("Migration failed: %v", err)
 	}
 	log.Println("Migration did run successfully")
+}
+
+func MigrateAddUserProfile20230719() *gormigrate.Migration {
+	sql := "CREATE TABLE `jnoj`.`user_profile`(" +
+		"`user_id` INT NOT NULL," +
+		"`realname` VARCHAR(64) NOT NULL DEFAULT ''," +
+		"`location` VARCHAR(128) NOT NULL DEFAULT ''," +
+		"`bio` VARCHAR(512) NOT NULL DEFAULT ''," +
+		"`gender` TINYINT UNSIGNED NOT NULL DEFAULT '0'," +
+		"`school` VARCHAR(255) NOT NULL DEFAULT ''," +
+		"`birthday` DATETIME NULL DEFAULT NULL," +
+		"`company` VARCHAR(255) NOT NULL DEFAULT ''," +
+		"`job` VARCHAR(255) NOT NULL DEFAULT ''," +
+		"`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP," +
+		"`updated_at` DATETIME ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP," +
+		"PRIMARY KEY(`user_id`)" +
+		") ENGINE = InnoDB CHARSET = utf8mb4 COLLATE utf8mb4_general_ci;"
+	return &gormigrate.Migration{
+		ID: "MigrateAddUserProfile20230719",
+		Migrate: func(d *gorm.DB) error {
+			err := d.Exec(sql).Error
+			if err != nil {
+				return err
+			}
+			// 迁移旧数据
+			var users []struct {
+				ID       int
+				Realname string
+			}
+			d.Select("id, realname").Table("user").Find(&users)
+			for _, v := range users {
+				err := d.Exec("INSERT INTO user_profile (user_id, realname) VALUES (?, ?)", v.ID, v.Realname).Error
+				if err != nil {
+					return err
+				}
+			}
+			return d.Exec("ALTER TABLE `user` DROP `realname`").Error
+		},
+		Rollback: func(d *gorm.DB) error {
+			return d.Exec("DROP TABLE `user_profile").Error
+		},
+	}
 }
 
 func MigrateAddUserBadge20230717() *gormigrate.Migration {
