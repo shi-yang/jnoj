18bbc5cdf1c195fb8a5f8d6623ba65598a5cfec0
shiyang <dr@shiyang.me>
2023-07-28 20:47:19 +0800

    fix: markdown 无法渲染表格

--- web/src/components/MarkdownView/index.tsx
+++ web/src/components/MarkdownView/index.tsx
@@ -1,15 +1,18 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import rehypeHighlight from 'rehype-highlight';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import 'katex/dist/katex.min.css';
 import 'highlight.js/styles/vs.css';
+import 'github-markdown-css/github-markdown.css';
 
 export default function Markdown({content}: {content: string}) {
   return (
     <ReactMarkdown
-      remarkPlugins={[remarkMath]}
+      className='markdown-body'
+      remarkPlugins={[remarkGfm, remarkMath]}
       rehypePlugins={[rehypeHighlight, rehypeKatex]}
     >
       {content}
